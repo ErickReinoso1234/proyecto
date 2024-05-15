ac5019cc178bfd978823eb37e26191c339c9a8e0
Erick Reinoso <eds.reinoso@yavirac.edu.ec>
2024-05-14 20:46:12 -0500

    Pagina 404

---
+++ src/pages/error/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Error404Page = () => {
+  return (
+    <div>
+      <h1 className="text-white">Error 404</h1>
+      <p className="text-white">La página que estás buscando no se encontró.</p>
+     
+    </div>
+  );
+};
+
+export default Error404Page;
--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,15 @@
-import React from "react"
-import { Route, Routes } from "react-router-dom"
-import AhorroApp from "./pages/ahorro/ahorros"
-import PaginaPrincipal from "./pages/paginaPrincipal/paginaPrincipal"
-export const AppRouter : React.FC <{}> = ()=>{
-    return (
-        <Routes>
-            <Route path="/" element= {<PaginaPrincipal></PaginaPrincipal>}/>
-            <Route path="/ahorros" element= {<AhorroApp></AhorroApp>}/>
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AhorroApp from "./pages/ahorro/ahorros";
+import PaginaPrincipal from "./pages/paginaPrincipal/paginaPrincipal";
+import Error404Page from "./pages/error";
 
-        </Routes>
-    )
-
-}
+export const AppRouter: React.FC<{}> = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<PaginaPrincipal />} />
+      <Route path="/ahorros" element={<AhorroApp />} />
+      <Route path="*" element={<Error404Page />} />
+    </Routes>
+  );
+};
