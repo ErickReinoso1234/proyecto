1e87bbdd86fbcb7dd2341116bb137ef8ed8bc296
users <jam.altamirano@yavirac.edu.ec>
2024-05-15 21:49:44 -0500

    Realizavion de sidebar-navbar y pagina de inicio de la aplicacion y actualizaciones

--- src/pages/perfilUsuario/perfil.tsx
+++ src/pages/perfilUsuario/perfil.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const perfilUsuario: React.FC = () =>{
+const perfilUsuarios: React.FC = () =>{
    return(
        <div></div>
    )
 }
-export default perfilUsuario;
+export default perfilUsuarios;
