82bdaa987395a071d3c101e3c103f6db582cee7c
Erick Reinoso <eds.reinoso@yavirac.edu.ec>
2024-05-16 10:38:26 -0500

    perfil de usuario y login

--- src/router.tsx
+++ src/router.tsx
@@ -4,14 +4,19 @@ import AhorroApp from "./pages/ahorro/ahorros";
 import PaginaPrincipal from "./pages/paginaPrincipal/paginaPrincipal";
 import Error404Page from "./pages/error";
 import Historial from "./pages/historial";
+import PerfilUsuario from "./pages/perfilUsuario/perfil";
+import LoginForm from "./pages/login";
 
 export const AppRouter: React.FC<{}> = () => {
   return (
     <Routes>
       <Route path="/" element={<PaginaPrincipal />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/perfil" element={<PerfilUsuario />} />
       <Route path="/ahorros" element={<AhorroApp />} />
       <Route path="/historial" element={<Historial />} />
       <Route path="*" element={<Error404Page />} />
+      <Route path="/login" element={<LoginForm />} />
     </Routes>
   );
 };
